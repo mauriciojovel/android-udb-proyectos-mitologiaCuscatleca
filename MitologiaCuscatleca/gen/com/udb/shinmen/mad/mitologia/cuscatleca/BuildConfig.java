34f47459ef789d3325d2a79fa71e977e82a8ed42
mauriciojovel <mauriciojovel@gmail.com>
2013-07-26 19:22:25 -0600

    Inicio del proyecto

---
+++ MitologiaCuscatleca/gen/com/udb/shinmen/mad/mitologia/cuscatleca/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.udb.shinmen.mad.mitologia.cuscatleca;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
