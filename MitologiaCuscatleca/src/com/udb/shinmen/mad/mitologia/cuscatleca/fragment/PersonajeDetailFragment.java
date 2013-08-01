f07152e5b38b49ea4eaaf8f4dcab0845aa674851
shinmen14 <shinmen14@gmail.com>
2013-08-01 09:21:19 -0600

    Implementación de WebView para el contenido de la leyenda. La imagen se muestra en linea con el texto.

--- MitologiaCuscatleca/src/com/udb/shinmen/mad/mitologia/cuscatleca/fragment/PersonajeDetailFragment.java
+++ MitologiaCuscatleca/src/com/udb/shinmen/mad/mitologia/cuscatleca/fragment/PersonajeDetailFragment.java
@@ -1,14 +1,11 @@
 package com.udb.shinmen.mad.mitologia.cuscatleca.fragment;
 
-
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.drawable.Drawable;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.Html;
-import android.text.Html.ImageGetter;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,20 +13,22 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import com.udb.shinmen.mad.mitologia.cuscatleca.R;
 import com.udb.shinmen.mad.mitologia.cuscatleca.SQLiteHelper.PersonajeSQLiteOpenHelper;
 import com.udb.shinmen.mad.mitologia.cuscatleca.constant.DB;
 
-public class PersonajeDetailFragment extends Fragment implements OnClickListener{
-	
+public class PersonajeDetailFragment extends Fragment implements
+		OnClickListener {
+
 	static final String CURR_POS_DETAIL = "com.udb.shinmen.mad.mitologia"
 			+ ".cuscatleca.fragment.PersonajeDetailFragment.CURR_POS_DETAIL";
 	static final String DUAL_PANE = "com.udb.shinmen.mad.mitologia"
-            + ".cuscatleca.fragment.PersonajeDetailFragment.DUAL_PANE";
+			+ ".cuscatleca.fragment.PersonajeDetailFragment.DUAL_PANE";
 	PersonajeSQLiteOpenHelper personajeSQLiteOpenHelper;
-	
+
 	public static PersonajeDetailFragment newInstance(long id, boolean dualPane) {
 		PersonajeDetailFragment p = new PersonajeDetailFragment();
 		Bundle b = new Bundle();
@@ -38,129 +37,124 @@ public class PersonajeDetailFragment extends Fragment implements OnClickListener
 		p.setArguments(b);
 		return p;
 	}
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		TextView textView = null;
-		View v = inflater.inflate(R.layout.activity_personaje_detail
-												, container, false);
-		personajeSQLiteOpenHelper = 
-				new PersonajeSQLiteOpenHelper(getActivity());
+		WebView webView = null;
+		View v = inflater.inflate(R.layout.activity_personaje_detail,
+				container, false);
+		personajeSQLiteOpenHelper = new PersonajeSQLiteOpenHelper(getActivity());
 		Cursor data = personajeSQLiteOpenHelper.get(getIndex());
 		String pathImg;
-        
-        
-		if(data.moveToFirst()) {
-		    //Nombre personaje
+
+		if (data.moveToFirst()) {
+			// Nombre personaje
 			textView = (TextView) v.findViewById(R.id.txvNombre);
 			textView.setText(data.getString(DB.Personaje.nombre.ordinal()));
-			if(isDualPane()) {
-			    textView.setVisibility(View.GONE);
+			if (isDualPane()) {
+				textView.setVisibility(View.GONE);
 			}
-			//Sipnosis
-			textView = (TextView) v.findViewById(R.id.txvSipnosis);
-			//textView.setText(data.getString(DB.Personaje.sipnosis.ordinal()));
+			// Sipnosis
+			webView = (WebView) v.findViewById(R.id.wvSipnosis);
 			String html = data.getString(DB.Personaje.sipnosis.ordinal());
-
 			pathImg = data.getString(DB.Personaje.ruta_imagen.ordinal());
-			if(pathImg != null && !pathImg.trim().equals("")) {
-                html = "<div><img src=\""+pathImg+"\"/>" + html + "</div>";
-            } else {
-                html = "<div>" + html + "</div>";
-            }
-			textView.setText(Html.fromHtml(html, new ImageGetter() {
-	               
-                @Override
-                public Drawable getDrawable(String source) {
-                    Drawable d = Drawable.createFromPath(source);
-                    d.setBounds(0,0,75,75);
-                    return d;
-                }
-            }, null));
-			//Link interes
+			
+			html = "<p style=\"word-wrap: break-word; \">" + html + "</p>";
+			
+			if (pathImg != null && !pathImg.trim().equals("")) {
+				html = "<img style=\"clear:right; float: left; margin: 10px\" src=\"file://" + pathImg
+						+ "\" align=\"middle\" height=\"100\" width=\"100\"/>" + html;
+			}
+
+			webView.getSettings().setSupportZoom(false);
+			webView.getSettings().setJavaScriptEnabled(false);
+			webView.loadDataWithBaseURL(null, html, "text/html", "utf-8", null);
+			webView.setBackgroundColor(Color.parseColor(getResources().getString(R.color.lightPaper)));
+			// Link interes
 			textView = (TextView) v.findViewById(R.id.txvLinkInteres);
-            textView.setText(data.getString(DB.Personaje
-                                                    .link_interes.ordinal()));
-            textView.setOnClickListener(this);
+			textView.setText(data.getString(DB.Personaje.link_interes.ordinal()));
+			textView.setOnClickListener(this);
 		}
 		data.close();
 		personajeSQLiteOpenHelper.close();
 		setHasOptionsMenu(true);
 		return v;
 	}
-	
+
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-	    super.onCreateOptionsMenu(menu, inflater);
-	    getActivity().getMenuInflater()
-	                    .inflate(R.menu.context_share_menu, menu);
+		super.onCreateOptionsMenu(menu, inflater);
+		getActivity().getMenuInflater()
+				.inflate(R.menu.context_share_menu, menu);
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-	    return onContextItemSelected(item);
+		return onContextItemSelected(item);
 	}
-	
+
 	@Override
 	public boolean onContextItemSelected(MenuItem item) {
-	    switch (item.getItemId()) {
-        case R.id.action_personaje_share:
-            personajeSQLiteOpenHelper = 
-            new PersonajeSQLiteOpenHelper(getActivity());
-            Cursor data = personajeSQLiteOpenHelper.get(getIndex());
-            String nombre = "-Ups-";
-            String sipnosis = "Esto no debio de haber pasado";
-            if(data.moveToFirst()) {
-                nombre = data.getString(DB.Personaje.nombre.ordinal());
-                sipnosis = data.getString(DB.Personaje.sipnosis.ordinal());
-            }
-            data.close();
-            callShare(nombre, sipnosis);
-            break;
+		switch (item.getItemId()) {
+		case R.id.action_personaje_share:
+			personajeSQLiteOpenHelper = new PersonajeSQLiteOpenHelper(
+					getActivity());
+			Cursor data = personajeSQLiteOpenHelper.get(getIndex());
+			String nombre = "-Ups-";
+			String sipnosis = "Esto no debio de haber pasado";
+			if (data.moveToFirst()) {
+				nombre = data.getString(DB.Personaje.nombre.ordinal());
+				sipnosis = data.getString(DB.Personaje.sipnosis.ordinal());
+			}
+			data.close();
+			callShare(nombre, sipnosis);
+			break;
 
-        default:
-            break;
-        }
-	    return super.onContextItemSelected(item);
+		default:
+			break;
+		}
+		return super.onContextItemSelected(item);
 	}
-	
+
 	public long getIndex() {
 		return getArguments().getLong(CURR_POS_DETAIL);
 	}
-	
+
 	public boolean isDualPane() {
-	    return getArguments().getBoolean(DUAL_PANE);
+		return getArguments().getBoolean(DUAL_PANE);
 	}
-	
+
 	public void callWebBrowser(String url) {
-	    if(url != null && !url.trim().equals("")) {
-            Intent callIntent = new Intent(Intent.ACTION_VIEW);
-            if(url.startsWith("http")) {
-                callIntent.setData(Uri.parse(url));
-            } else {
-                callIntent.setData(Uri.parse("http://"+url));
-            }
-            startActivity(callIntent);
-	    }
+		if (url != null && !url.trim().equals("")) {
+			Intent callIntent = new Intent(Intent.ACTION_VIEW);
+			if (url.startsWith("http")) {
+				callIntent.setData(Uri.parse(url));
+			} else {
+				callIntent.setData(Uri.parse("http://" + url));
+			}
+			startActivity(callIntent);
+		}
 
 	}
-	
+
 	public void callShare(String nombrePersonaje, String sipnosis) {
-	    Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-        sharingIntent.setType("text/plain");
-        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, nombrePersonaje);
-        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, sipnosis);
-        startActivity(Intent.createChooser(sharingIntent
-                , getResources().getString(R.string.compartir)));
+		Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+		sharingIntent.setType("text/plain");
+		sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
+				nombrePersonaje);
+		sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, sipnosis);
+		startActivity(Intent.createChooser(sharingIntent, getResources()
+				.getString(R.string.compartir)));
 	}
 
-    @Override
-    public void onClick(View v) {
-        if (v instanceof TextView) {
-            TextView t = (TextView) v;
-            callWebBrowser(t.getText().toString());
-        }        
-    }
-	
+	@Override
+	public void onClick(View v) {
+		if (v instanceof TextView) {
+			TextView t = (TextView) v;
+			callWebBrowser(t.getText().toString());
+		}
+	}
+
 }
